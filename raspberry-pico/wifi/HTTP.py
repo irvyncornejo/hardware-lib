41c300f425b3e654652a4db8cd5ee3ce99792911
Irvyn Cornejo <irvyncornejo@gmail.com>
2023-03-19 23:18:56 -0600

    Add new function to WLAN config

--- raspberry-pico/wifi/HTTP.py
+++ raspberry-pico/wifi/HTTP.py
@@ -1,28 +1,38 @@
+from time import sleep
 import network
 import urequests
 
-from config import ssid, password
-
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-ssid = ssid
-password = password
-wlan.connect(ssid, password)
 
 class HTTP:
     """
         Use Only with raspberry pi pico w
     """
-    def __init__(self) -> None:
+    def __init__(self, ssid, password) -> None:
+        self._connect(ssid, password)
         self._response = None
         self._format = 'json'
+    
+    def _set_url(self, url:str):
+        pass
+
+    def _connect(self, ssid:str, password:str)->str:
+        wlan = network.WLAN(network.STA_IF)
+        wlan.active(True)
+        wlan.connect(ssid, password)
+        while wlan.isconnected() == False:
+            print('Waiting for connection..')
+            sleep(1)
+        ip = wlan.ifconfig()[0]
+        print(f'Connected on ip -> {ip}')
+        return ip
+            
 
     def _format_return(self):
         if self._format == 'json':
             return self._response.json()
         return self._response
 
-    def get(self, url: str, format_response='json'):
+    def get(self, url: str, format_response:str='json'):
         print(f'Get {url}')
         self._response = urequests.get(url)
         return self._format_return()
